9e8ced3f36088fb55adeb668e072d315cc503237
Jackson Uwadiae <Jacksuw@gmail.com>
2021-09-29 01:32:22 +0000

    Modified card-game.php and cleared format

--- practice/card-game.php
+++ practice/card-game.php
@@ -1,29 +1,24 @@
 <?php
-$cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-
-
-//$playerCards = array_splice($cards, 0, count($cards) / 2);
-//$computerCards = $cards;
-//You will likely (but are not required to) use a combination of a foreach and if construct.
 //var_dump($playerCards);
 //var_dump($computerCards);
 
-foreach ($cards as $card) {
-    shuffle($cards);
-    if (count($cards) != 0) {
-        $playerCards = array_pop($cards);
-        $computerCards = array_pop($cards);
-        var_dump("player1");
-        var_dump($playerCards);
-        var_dump("computerCard");
-        var_dump($computerCards);
+$cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+shuffle($cards);
+
+$playerCards = [];
+$computerCards = [];
+
+
+foreach ($cards as $key => $card) {
+    $cardToDeal = array_pop($cards);
+
+    if ($key % 2 == 0) {
+        $playerCards[] = $cardToDeal;
+    } else {
+        $computerCards[] = $cardToDeal;
     }
 }
-// $total = rand(0, 100);
-// var_dump($total);
 
-// var_dump($total <= 30 or $total > 50);
-// var_dump($ansL);
-
-// var_dump(1 == 1 && 5 < 2);
+var_dump($playerCards);
+var_dump($computerCards);
