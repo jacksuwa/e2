cb0814c057a3d5953ceb26c06804dde31334a3c4
Jackson Uwadiae <Jacksuw@gmail.com>
2021-10-05 01:41:30 +0000

    Modified project 1 index.php and Readme.md

--- p1/README.md
+++ p1/README.md
@@ -5,9 +5,9 @@
 ## Game planning
 + Create an array, with three elements: Rock, Paper or Scissors  
 + For Player A choose a random element from Rock, Paper or Scissors  
-+ For Player B choose a random element from Rock, Paper or Scissors  
++ For Player B choose a random element from Rock, Paper or Scissors 
 + A tie is declared if both players chooses the same random element 
-+ Otherwise: Winner based on Element selected by either Player A or Player B.  
++ Otherwise: Winner is based on element selected by Player A or Player B.  
 + Element Rock beats Scissors, Scissors beats Paper, Paper beats Rock. 
 + In the view, show the results of Player a's move, player b's move and the winner 
 
--- p1/index.php
+++ p1/index.php
@@ -2,5 +2,23 @@
 
 //+ Create an array, with three elements: Rock, Paper or Scissors
 $moves = ['rock', 'paper', 'scissors'];
+$player1Move = $moves[rand(0, 2)];
+$player2Move = $moves[rand(0, 2)];
+
+var_dump($player1Move);
+var_dump($player2Move);
+
+if ($player1Move == $player2Move) {
+    var_dump('Tie');
+} elseif ($player1Move == 'rock' and $player2Move == 'paper') {
+    var_dump("Player 2 is the Winner");
+} elseif ($player1Move == 'rock' and $player2Move == 'scissors') {
+    var_dump('Player 1 Wins');
+} elseif ($player1Move == 'paper' and $player2Move == 'rock') {
+    var_dump('Player 1 Wins');
+} elseif ($player1Move == 'paper' and $player2Move == 'scissors') {
+    var_dump('Player 2 Wins');
+}
+
 
 require 'index-view.php';
