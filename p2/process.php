8ee334a14adf147805ff9382fb9773be77191970
Jackson Uwadiae <Jacksuw@gmail.com>
2021-10-25 03:34:43 +0000

    Update to Project 2 Code

--- p2/process.php
+++ p2/process.php
@@ -20,25 +20,3 @@ if ($choice == $moves[0] and $computer == $moves[1]) {
     var_dump($computer);
     var_dump('Tis a tie!');
 }
-
-
-
-
-
-
-// if ($computer == $moves[0] and $choice == $moves[1]) {
-//     echo $winner = true;
-// } elseif ($comp == $moves[0] and $choice == $moves[2]) {
-//     echo $winner;
-// }
-
-
-// } elseif ($comp == $moves[1] and $choice == $moves[0]) {
-//     echo $winner;
-// } elseif ($comp == $moves[1] and $choice == $moves[2]) {
-//     echo $winner = true;
-// } elseif ($comp == $moves[2] and $choice == $moves[1]) {
-//     echo $winner;
-// } elseif ($comp == $moves[2] and $choice == $moves[0]) {
-//     echo $winner = true;
-// }
