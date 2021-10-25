af5bd8ab2b5d36d70416de5acff4378e926cdbc1
Jackson Uwadiae <Jacksuw@gmail.com>
2021-10-25 03:27:28 +0000

    Updated Project 2 Code

---
+++ p2/index-view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Project 1 - Rock, Paper, Scissors</title>
+    <meta charset='utf-8'>
+
+</head>
+
+<body>
+    <h1>Project 1 - Rock, Paper, Scissors</h1>
+
+    <form method='POST' action='process.php'>
+        <input type='radio' id='rock' name='move' value='rock' checked><label for='rock'>Rock</label>
+        <input type='radio' id='paper' name='move' value='paper'><label for='paper'>Paper</label>
+        <input type='radio' id='scissors' name='move' value='scissors'><label for='scissors'>Scissors</label>
+
+
+        <button type='submit'>Play</button>
+
+    </form>
+</body>
+
+</html>
