b7822c32c14bbb857d81d2e8f102715578170711
Jackson Uwadiae <Jacksuw@gmail.com>
2021-10-27 00:56:52 +0000

    Modified index-view

--- p2/index-view.php
+++ p2/index-view.php
@@ -3,13 +3,28 @@
 
 <head>
 
-    <title>Project 1 - Rock, Paper, Scissors</title>
+    <title>Project 2 - Rock, Paper, Scissors</title>
     <meta charset='utf-8'>
     <link rel="stylesheet" href="styles/p2.css" />
 </head>
 
 <body>
-    <h1>Project 1 - Rock, Paper, Scissors</h1>
+    <h1>Project 2 - Rock, Paper, Scissors</h1>
+
+    <h2>Mechanics</h2>
+    <ul>
+        <li>
+            Player A (You) and Player B (Computer) select between items: Rock, Paper or Scissors.
+        </li>
+        <li>
+            The game will be a tie if both players select the same item.
+        </li>
+        <li>
+            Otherwise: Rock beats Scissors, Paper beats Rock, and Scissors beats Paper.
+        </li>
+    </ul>
+
+
     <form method='POST' action='process.php'>
         <input type='radio' id='rock' name='choice' value='rock'
             <?php echo (!isset($choice) or $choice == 'rock') ? 'checked' : '' ?>><label for='rock'>Rock</label>
