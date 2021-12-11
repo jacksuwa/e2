f0c88705fe03505b2cd2eb6cfd0f82d096f4108b
Jackson Uwadiae <Jacksuw@gmail.com>
2021-12-11 11:49:55 +0000

    Made changes to Project 3 code

--- p3/app/Controllers/AppController.php
+++ p3/app/Controllers/AppController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controllers;
 
 class AppController extends Controller
@@ -9,9 +10,13 @@ class AppController extends Controller
     public function index()
     {
         $welcomes = ['Welcome', 'Aloha', 'Welkom', 'Bienvenidos', 'Bienvenu', 'Welkomma'];
-        
+
         return $this->app->view('index', [
             'welcome' => $welcomes[array_rand($welcomes)]
         ]);
+    }
+    public function process()
+    {
+        dump($this->app->inputAll());
     }
 }
--- p3/views/index.blade.php
+++ p3/views/index.blade.php
@@ -1,13 +1,27 @@
 @extends('templates/master')
 
-@section('title')
-    {{ $welcome }}
-@endsection
+
 
 @section('content')
+    <h2>Instructions</h2>
+    <ul>
+        <li>
+            Player A (You) and Player B (Computer) select between items: Rock, Paper or Scissors.
+        </li>
+        <li>
+            The game will be a tie if both players select the same item.
+        </li>
+        <li>
+            Otherwise: Rock beats Scissors, Paper beats Rock, and Scissors beats Paper.
+        </li>
+    </ul>
 
-    <h2>{{ $welcome }}</h2>
+    <form method='POST' action='/process'>
 
-    <p>Hello and welcome!</a>.</p>
+        <input type='radio' id='rock' name='choice' value='rock'><label for='rock'>Rock</label>
+        <input type='radio' id='paper' name='choice' value='paper'><label for='paper'>Paper</label>
+        <input type='radio' id='scissors' name='choice' value='scissors'><label for='scissors'>Scissors</label>
 
+        <button type='submit'>Play</button>
+    </form>
 @endsection
